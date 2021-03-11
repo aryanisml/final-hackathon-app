48caeac178ddb122033643d389fcdb4b537407a6
swapnil.kamble <swapnil.kamble@us.imshealth.com>
2021-03-11 19:06:22 +0530

    Edit functionality

--- src/app/beneficiary/edit-benficairies/edit-benficairies.component.ts
+++ src/app/beneficiary/edit-benficairies/edit-benficairies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit, Renderer2, Input, OnDestroy } from '@angular/core';
+import { AbstractControlOptions, AsyncValidatorFn, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -10,16 +12,25 @@ import { BeneficiaryService } from '../beneficiary.service';
   styleUrls: ['./edit-benficairies.component.scss']
 })
 export class EditBenficairiesComponent implements OnInit, OnDestroy {
-
+  custCreateForm!: FormGroup;
+  submitted = false;
+  validPattern: any;
+  bankName: any;
+  isAlive: any;
   private destory$: Subject<boolean> = new Subject<boolean>();
   updateFav: any;
+  acnId: number | undefined;
+  get f() { return this.custCreateForm.controls; }
   constructor(
     private beneficiaryService: BeneficiaryService,
-    private router: Router) {
+    private router: Router,
+    private formBuilder: FormBuilder) {
+    this.createForm();
   }
 
   ngOnInit(): void {
     this.fetchData();
+
   }
 
   ngOnDestroy(): void {
@@ -48,10 +59,35 @@ export class EditBenficairiesComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destory$))
       .subscribe(response => {
         if (response) {
-          console.log(response);
+          console.log(this.f);
+          this.acnId = response.id;
+          this.setData(response);
         }
       });
   }
 
+  createForm() {
+    this.custCreateForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      accountnumber: ['', [Validators.required, Validators.minLength(20)]],
+      bank: [''],
+    });
+  }
+  onSubmit() {
+    let accountDetails = this.custCreateForm.value;
+    accountDetails = {
+      ...accountDetails,
+      id: this.acnId
+    }
+    this.beneficiaryService.editBeneficiaries(accountDetails)
+      .subscribe(() => {
+        this.router.navigate([`ibank`]);
+      });
+  }
 
+
+  setData(response?: any) {
+
+    this.custCreateForm.patchValue(response, { emitEvent: false });
+  }
 }
